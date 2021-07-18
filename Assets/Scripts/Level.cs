c67aea70b45c246ffafc786e5ba10222dd1717bd
Владимир <smirnov.vladimir17@gmail.com>
2021-07-18 21:22:55 +0300

    feat: update

--- Assets/Scripts/Level.cs
+++ Assets/Scripts/Level.cs
@@ -5,108 +5,139 @@ using UnityEngine;
 
 // Требования
 // Мир представлен сеткой
-// Два слоя сетки - земля и объекты
+// Три слоя сетки - земля, объекты и окружение
 // Перемещение объектов - мгновенно
 // Добавление и удаление объектов и земли
 
-public enum Layer
+class Cell
 {
-    Background,
-    Foreground,
+    public Cell(GameObject ground, GameObject o)
+    {
+        Ground = ground;
+        Object = o;
+    }
+
+    public GameObject Ground;
+    public GameObject Object;
 }
 
 public class Level : MonoBehaviour
 {
     public float sellSize = 1;
-    public float groundHight = 1;
-    public GameObject testObject = null;
-    public GameObject testCell = null;
 
-    public void Add(Vector2 pos, GameObject obj, Layer layer = Layer.Foreground)
+    public void ToGridCoords(GameObject obj, Vector2 gridCoords)
     {
-        switch (layer)
-        {
-            case Layer.Background:
-                background.Add(pos, obj);
-                break;
-            case Layer.Foreground:
-                foreground.Add(pos, obj);
-                break;
-            default:
-                throw new ArgumentOutOfRangeException(nameof(layer), layer, null);
-        }
-
-        Move(pos, pos, layer);
+        var gridTo = gridCoords * sellSize;
+        obj.transform.position = new Vector3(gridTo.x, obj.transform.position.y, gridTo.y);
     }
 
-    public void Remove(Vector2 pos, Layer layer = Layer.Foreground)
+    public void AddGround(Vector2 pos, GameObject obj)
     {
-        switch (layer)
+        if (cells.TryGetValue(pos, out var cell))
         {
-            case Layer.Background:
-                background.Remove(pos);
-                break;
-            case Layer.Foreground:
-                foreground.Remove(pos);
-                break;
-            default:
-                throw new ArgumentOutOfRangeException(nameof(layer), layer, null);
+            Destroy(cell.Ground);
+            cell.Ground = obj;
+        }
+        else
+        {
+            cells.Add(pos, new Cell(obj, null));
+        }
+        
+        ToGridCoords(obj, pos);
+    }
+
+    public void AddObject(Vector2 pos, GameObject obj)
+    {
+        if (!cells.TryGetValue(pos, out var cell))
+        {
+            throw new Exception("Object can not exist without ground!");
+        }
+
+        cell.Object = obj;
+        ToGridCoords(obj, pos);
+    }
+
+    public void AddAmbient(Vector2 pos, GameObject obj)
+    {
+        if (ambients.TryGetValue(pos, out var ambient))
+        {
+            Destroy(ambient);
+        }
+        ambients.Add(pos, obj);
+        ToGridCoords(obj, pos);
+    }
+
+    public void RemoveObject(Vector2 pos)
+    {
+        var cell = cells[pos];
+        if (layer == Layer.Ground)
+        {
+            if (cell.Object != null)
+            {
+                Destroy(cell.Object);
+            }
+
+            cells.Remove(pos);
+        }
+        else if (layer == Layer.Object)
+        {
+            Destroy(cell.Object);
+        }
+        else if (layer == Layer.Ambient)
+        {
+            Destroy(ambients[pos]);
+            ambients.Remove(pos);
+        }
+        else
+        {
+            throw new ArgumentOutOfRangeException(nameof(layer), layer, null);
         }
 
         Destroy(Get(pos, layer));
     }
 
-    public void Move(Vector2 from, Vector2 to, Layer layer = Layer.Foreground)
+    public void Move(Vector2 from, Vector2 to, Layer layer = Layer.Object)
     {
         var gridTo = to * sellSize;
         var obj = Get(from, layer);
         switch (layer)
         {
-            case Layer.Background:
+            case Layer.Ground:
                 obj.transform.position = new Vector3(gridTo.x, 0, gridTo.y);
-                Library.RenameKey(background, from, to);
+                Library.RenameKey(grounds, from, to);
                 break;
-            case Layer.Foreground:
+            case Layer.Object:
                 obj.transform.position = new Vector3(gridTo.x, groundHight, gridTo.y);
-                Library.RenameKey(foreground, from, to);
+                Library.RenameKey(objects, from, to);
+                break;
+            case Layer.Ambient:
+                obj.transform.position = new Vector3(gridTo.x, groundHight, gridTo.y);
+                Library.RenameKey(ambients, from, to);
                 break;
             default:
                 throw new ArgumentOutOfRangeException(nameof(layer), layer, null);
         }
     }
 
-    public GameObject Get(Vector2 pos, Layer layer = Layer.Foreground)
+    public GameObject Get(Vector2 pos, Layer layer = Layer.Object)
     {
         return layer switch
         {
-            Layer.Background => background[pos],
-            Layer.Foreground => foreground[pos],
+            Layer.Ground => cells.TryGetValue(pos, out var cellValue) ? cellValue.Ground : null,
+            Layer.Object => cells.TryGetValue(pos, out var cellValue) ? cellValue.Object : null,
+            Layer.Ambient => ambients.TryGetValue(pos, out var cellValue) ? cellValue : null,
             _ => throw new ArgumentOutOfRangeException(nameof(layer), layer, null)
         };
     }
 
     private void Start()
     {
-        background = new Dictionary<Vector2, GameObject>();
-        foreground = new Dictionary<Vector2, GameObject>();
-
-        for (int i = 0; i < 10; i++)
-        {
-            for (int j = 0; j < 20; j++)
-            {
-                Add(new Vector2(i, j), Instantiate(testCell), Layer.Background);
-                if (i % 3 == 0 && j % 3 == 0)
-                {
-                    Add(new Vector2(i, j), Instantiate(testObject), Layer.Foreground);
-                }
-            }
-        }
     }
 
     private void Update()
     {
     }
 
-    private Dictionary<Vector2, GameObject> background;
-    private Dictionary<Vector2, GameObject> foreground;
+    private Dictionary<Vector2, Cell> cells = new Dictionary<Vector2, Cell>();
+    private Dictionary<Vector2, GameObject> ambients = new Dictionary<Vector2, GameObject>();
 }
